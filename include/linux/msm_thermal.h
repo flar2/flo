c3c47a767255d0b90a87563021388db0a2617fe7
flar2 <asegaert@gmail.com>
2013-08-13 23:15:11 -0400

    kernel-based thermal control (based on @crpalmer's work)
    Signed-off-by: flar2 <asegaert@gmail.com>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -14,12 +14,14 @@
 #ifndef __MSM_THERMAL_H
 #define __MSM_THERMAL_H
 
+extern int throttled_bin;
+
 struct msm_thermal_data {
 	uint32_t sensor_id;
-	uint32_t poll_ms;
+/*	uint32_t poll_ms;
 	uint32_t limit_temp_degC;
 	uint32_t temp_hysteresis_degC;
-	uint32_t freq_step;
+	uint32_t freq_step;*/
 };
 
 #ifdef CONFIG_THERMAL_MONITOR
