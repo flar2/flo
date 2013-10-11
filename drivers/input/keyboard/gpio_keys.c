eb0f23663b14a5b8726773956ae893623d0d49be
flar2 <asegaert@gmail.com>
2013-10-10 23:39:57 -0400

    New sweep2wake/doubletap2wake options
    /sys/android_touch/orientation controls whether sweep2sleep works in portrait, landscape, or both
    0 = both
    1 = portrait only
    2 = landscape only
    
    /sys/android_touch/pwrkey_suspend  makes the power button toggle s2w/dt2w
    If set to 1, pressing power button to turn off screen disables s2w/dt2w and pressing the power button to turn on screen re-enables s2w/dt2w
    
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -32,6 +32,8 @@
 
 #include <linux/sweep2wake.h>
 
+int pwr_key_pressed = 0;
+
 struct gpio_button_data {
 	const struct gpio_keys_button *button;
 	struct input_dev *input;
@@ -372,6 +374,7 @@ static void gpio_keys_gpio_report_event(struct gpio_button_data *bdata)
 	} else {
 		input_event(input, type, button->code, !!state);
 	}
+
 	input_sync(input);
 }
 
@@ -389,6 +392,12 @@ static void gpio_keys_gpio_work_func(struct work_struct *work)
 	if ((button->code <= KEY_POWER) && (button->code >= KEY_VOLUMEDOWN))
 		pr_info("gpio_keys: %s %s\n", state ? "Pressed" : "Released",
 			key_descriptions[button->code - KEY_VOLUMEDOWN]);
+
+/*s2w*/
+	if (button->code == 116 && state) {
+		pwr_key_pressed = 1;
+	}
+
 #endif
 	if (button->can_reset) {
 		if (state)
