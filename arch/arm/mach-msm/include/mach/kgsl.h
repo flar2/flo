ccd87cbdb6242e96839dde1d5b5f05f0568700f9
flar2 <asegaert@gmail.com>
2013-12-13 00:16:06 -0500

    ElementalX governor
    Signed-off-by: flar2 <asegaert@gmail.com>

--- arch/arm/mach-msm/include/mach/kgsl.h
+++ arch/arm/mach-msm/include/mach/kgsl.h
@@ -43,6 +43,10 @@
 
 extern uint32_t max_gpu;
 
+#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
+extern int graphics_boost;
+#endif
+
 enum kgsl_iommu_context_id {
 	KGSL_IOMMU_CONTEXT_USER = 0,
 	KGSL_IOMMU_CONTEXT_PRIV = 1,
