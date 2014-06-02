53cad7b5eef1316dfef6b0b90f0b8e72954dce13
flar2 <asegaert@gmail.com>
2014-06-02 11:05:53 -0400

    Linaro GCC 4.9.1: fix compilation errors
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/slimbus/slimbus.c
+++ drivers/slimbus/slimbus.c
@@ -878,7 +878,7 @@ int slim_xfer_msg(struct slim_controller *ctrl, struct slim_device *sbdev,
 	int ret;
 	u16 sl, cur;
 	u16 ec;
-	u8 tid, mlen = 6;
+	u8 tid = 0, mlen = 6;
 
 	if (sbdev->laddr != SLIM_LA_MANAGER && sbdev->laddr >= ctrl->num_dev)
 		return -ENXIO;
