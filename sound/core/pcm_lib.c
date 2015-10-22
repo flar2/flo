cc04663efebac2cd22f57a660a96adc999e43a43
Lars-Peter Clausen <lars@metafoo.de>
2015-10-22 11:11:56 +0000

    ALSA: Remove transfer_ack_{begin,end} callbacks from struct snd_pcm_runtime
    While there is nothing wrong with the transfer_ack_begin and
    transfer_ack_end callbacks per-se, the last documented user was part of the
    alsa-driver 0.5.12a package, which was released 14 years ago and even
    predates the upstream integration of the ALSA core and has subsequently
    been superseded by newer alsa-driver releases.
    
    This seems to indicate that there is no need for having these callbacks and
    they are just cruft that can be removed.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 53e597b1d194910bef53ed0632da329fef497904)
    
    Change-Id: Ifa69c873640b171aa1843335b2b3cb856d29bb1a

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -1753,9 +1753,6 @@ void snd_pcm_period_elapsed(struct snd_pcm_substream *substream)
 		return;
 	runtime = substream->runtime;
 
-	if (runtime->transfer_ack_begin)
-		runtime->transfer_ack_begin(substream);
-
 	snd_pcm_stream_lock_irqsave(substream, flags);
 	if (!snd_pcm_running(substream) ||
 	    snd_pcm_update_hw_ptr0(substream, 1) < 0)
@@ -1765,8 +1762,6 @@ void snd_pcm_period_elapsed(struct snd_pcm_substream *substream)
 		snd_timer_interrupt(substream->timer, 1);
  _end:
 	snd_pcm_stream_unlock_irqrestore(substream, flags);
-	if (runtime->transfer_ack_end)
-		runtime->transfer_ack_end(substream);
 	kill_fasync(&runtime->fasync, SIGIO, POLL_IN);
 }
 
