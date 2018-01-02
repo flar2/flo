a63fd90f2118a6126ce10abdd6cca5579282214d
Artem Borisov <dedsa2002@gmail.com>
2018-01-02 19:16:43 +0300

    ARM: msm: flo: fix idle_timeout value to 100ms
    Change-Id: Ia4dd5f9d8fef504e8c6a67668bca9857e7958f61

--- arch/arm/mach-msm/asustek/flo/board-flo-gpu.c
+++ arch/arm/mach-msm/asustek/flo/board-flo-gpu.c
@@ -254,7 +254,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.init_level = 1,
 	.num_levels = 5,
 	.set_grp_async = NULL,
-	.idle_timeout = HZ/10,
+	.idle_timeout = 100,
 	.strtstp_sleepwake = true,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE | KGSL_CLK_MEM_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
